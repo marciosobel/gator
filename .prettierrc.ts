c2d59365e6203fab16cb8e21467f46722c307e4c
Márcio Sobel <marciosobel@proton.me>
2025-12-18 22:28:20 -0300

    feat: add prettier config

---
+++ .prettierrc.ts
@@ -0,0 +1,15 @@
+import type { Config } from "prettier";
+
+/** @see https://prettier.io/docs/configuration */
+const config: Config = {
+    tabWidth: 2,
+    semi: true,
+    overrides: [
+        {
+            files: "*.vue",
+            options: { tabWidth: 4 },
+        },
+    ],
+};
+
+export default config;
